cf7a5eada2e61a9eeda2c69940d71ac9956fe99b
simonhodne <nomis.hodne@gmail.com>
2024-04-27 14:10:40 +0200

    Log class file added

---
+++ Physical Game NUnitTest/LogTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using nanoFramework.TestFramework;
+
+namespace Physical_Game_NUnitTest
+{
+    [TestClass]
+    public class LogTests
+    {
+    }
+}
---
+++ Physical Game/Log.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Physical_Game
+{
+    public class Log
+    {
+    }
+}
