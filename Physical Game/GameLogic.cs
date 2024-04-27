910a185e1f39bb5f554cbb9a0a2c0c8ffd1c6171
simonhodne <nomis.hodne@gmail.com>
2024-04-27 21:42:06 +0200

    Timer fail condition added

--- Physical Game/GameLogic.cs
+++ Physical Game/GameLogic.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Threading;
-
+using System.Diagnostics;
 
 namespace Physical_Game
 {
     public class GameLogic
     {
+        #region Variables
+        
         HardwareInteraction hardware;
         Random random;
+        Stopwatch stopWatch;
         bool isRunning = true;
         int currentButton;
+        int currentTimeLimit = 2000;
+        long time;
+
+        #endregion
+
+        #region Functions
         public GameLogic(HardwareInteraction hardwareInstance)
         {
             hardware = hardwareInstance;
@@ -19,34 +28,45 @@ namespace Physical_Game
         
         void StartGame()
         {
-            hardware.ToggleLed(3);
+            hardware.ToggleLed(HardwareInteraction.CENTER_LED);
             Thread.Sleep(1000);
-            hardware.ToggleLed(3);
-            Thread.Sleep(500);
-            hardware.ToggleLed(3);
-            Thread.Sleep(500);
-            hardware.ToggleLed(3);
-            Thread.Sleep(500);
-            hardware.ToggleLed(3);
-            Thread.Sleep(500);
-            hardware.ToggleLed(3);
-            Thread.Sleep(500);
-            hardware.ToggleLed(3);
+            hardware.FlashLed(HardwareInteraction.CENTER_LED, 3, 500, hardware);
+            stopWatch = new();
             RunGame();
         }
         void RunGame()
         {
+            stopWatch.Start();
             while(isRunning)
             {
                 currentButton = random.Next(7);
                 hardware.ToggleLed(currentButton);
+                time = stopWatch.ElapsedMilliseconds;
+
                 if(hardware.GetInput() != currentButton)
                 {
                     isRunning = false;
                 }
+
+                if(time + currentTimeLimit < stopWatch.ElapsedMilliseconds)
+                {
+                    isRunning = false;
+                }
+                if(currentTimeLimit > 1000)
+                {
+                    currentTimeLimit -= 50;
+                }
                 hardware.ToggleLed(currentButton);
                 Thread.Sleep(500);
             }
+            EndGame();
         }
+        void EndGame()
+        {
+            stopWatch.Stop();
+            hardware.FlashLed(HardwareInteraction.TOGGLE_ALL, 2, 500, hardware);
+            hardware.ToggleLed(HardwareInteraction.CENTER_LED);
+        }
+        #endregion
     }
 }
